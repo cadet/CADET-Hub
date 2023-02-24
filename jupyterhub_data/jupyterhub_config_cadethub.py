d5b9f56d81efc74c49765e46610e4fdc48042baa
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2023-02-24 16:00:23 +0100

    Added basic docker swarm components
    - allows to spawn normal python notebooks as well as cadet server
    - docker-compose yaml with jupyterhub service
    - jupyterhub config with basic info
    - basic readme

---
+++ jupyterhub_data/jupyterhub_config_cadethub.py
@@ -0,0 +1,27 @@
+# Configuration file for jupyterhub.
+import os
+
+c = get_config()
+
+c.JupyterHub.spawner_class = "jhub.SwarmSpawner"
+
+c.JupyterHub.authenticator_class = "jhubauthenticators.DummyAuthenticator"
+c.DummyAuthenticator.password = "password"
+
+c.JupyterHub.ip = "0.0.0.0"
+c.JupyterHub.hub_ip = "0.0.0.0"
+
+# First pulls can be really slow, so let's give it a big timeout
+c.SwarmSpawner.start_timeout = 60 * 5
+c.SwarmSpawner.http_timeout = 60 * 1
+
+c.SwarmSpawner.notebook_dir = "~/work"
+c.SwarmSpawner.jupyterhub_service_name = "jupyterhub"
+c.SwarmSpawner.networks = ["cadethub_default"]
+
+c.SwarmSpawner.use_user_options = True
+
+c.SwarmSpawner.images = [
+    {"image": "ucphhpc/base-notebook:latest", "name": "Python Notebook"},
+    {"image": "cadet-test", "name": "Cadet Notebook"}
+]
